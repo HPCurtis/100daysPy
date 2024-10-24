6248fb252369045b2e586b295e00f582df2595d3
Harrison Curtis <harrisoncurtis50@gmail.com>
2024-10-24 21:43:53 +0100

    paddle start position fix

--- projects/intermediate/pong/main.py
+++ projects/intermediate/pong/main.py
@@ -7,11 +7,14 @@ screen.setup(width= 800, height = 600)
 screen.title("Pong")
 
 paddle = Turtle()
+
+paddle.penup()
+paddle.goto(350, 0)
+
 paddle.shape("square")
 paddle.shapesize(stretch_wid=5, stretch_len=1)
 paddle.color("white")
-paddle.penup()
-paddle.goto(350, 0)
+
 
 def go_up():
     new_y = paddle.ycor() + 20
