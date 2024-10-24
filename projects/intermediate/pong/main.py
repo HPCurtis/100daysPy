c224c75e369751a4ac73d8cf955d0cdc53130aa4
Harrison Curtis <harrisoncurtis50@gmail.com>
2024-10-24 17:47:32 +0100

    add pong directory

---
+++ projects/intermediate/pong/main.py
@@ -0,0 +1,9 @@
+from turtle import Screen
+
+# Set up the porjects screen
+screen = Screen()
+screen.bgcolor("black")
+screen.setup(width= 800, height = 600)
+screen.title("Pong")
+
+screen.exitonclick()
